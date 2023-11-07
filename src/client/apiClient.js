61a3937a13de347c18d71eefefb8d66f897a477c
sameh mohamed <sameh7898@gmail.com>
2023-11-07 19:52:53 +0200

    increase-timeout

--- src/client/apiClient.js
+++ src/client/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:8080/",
-  timeout: 7000,
+  timeout: 30000,
   headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
 });
 
